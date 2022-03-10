22f134dd94ce68fba20d36f345746f3e4ecc6c67
GGorAA <ggoraa1029@gmail.com>
2022-03-11 00:10:29 +0200

    feat: updated the approach for making lua api macros available in swift to a more portable approach

--- Sources/liblua/liblua.h
+++ Sources/liblua/liblua.h
@@ -14,30 +14,43 @@
 #include <lauxlib.h>
 #include <lualib.h>
 
-// Undefining macros...
+// Getting macros ready...
 
 /*
  This comment is for you, a guy that inspects a transpiled
  Swift header of this module. This section just
- contains #undef statements, and they don't have an alternative
- here in Swift, so that's why you don't see them in the Swift
- version.
+ contains #undef and #define statements, and they don't have an
+ alternative here in Swift, so that's why you don't see them in
+ the Swift version.
  */
+#define luakit_pushcfunction(L,f) lua_pushcfunction(L,f)
+#define luakit_pop(L,n) lua_pop(L,n)
+#define luakitL_checkstring(L,n) luaL_checkstring(L,n)
+
 #undef lua_pushcfunction(L,f)
 #undef lua_pop(L,n)
 #undef luaL_checkstring(L,n)
 
 // ...and replacing them with functions that Swift understands
 void lua_pushcfunction(lua_State *L, lua_CFunction fn) {
-    lua_pushcclosure(L, fn, 0);
+    luakit_pushcfunction(L, fn);
 }
 
 void lua_pop(lua_State *L, int idx) {
-    lua_settop(L, -(idx)-1);
+    luakit_pop(L, idx);
 }
 
 const char *luaL_checkstring(lua_State *L, int arg) {
-    luaL_checklstring(L, arg, NULL);
+    luakitL_checkstring(L, arg);
 }
+
+// also undefining non-needed stuff...
+
+/*
+ So yea, here is the same story with macros not being seen in Swift
+ */
+#undef luakit_pushcfunction(L,f)
+#undef luakit_pop(L,n)
+#undef luakitL_checkstring(L,n)
 
 #endif /* liblua_h */
