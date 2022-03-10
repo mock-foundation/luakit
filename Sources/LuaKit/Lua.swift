54ee71db7ca5a37fab94ce0d5b452aa3114991f3
GGorAA <ggoraa1029@gmail.com>
2022-03-10 22:57:48 +0200

    feat: a ton of updates to Lua class, and I am lazy to explain it here sooo

--- Sources/LuaKit/Lua.swift
+++ Sources/LuaKit/Lua.swift
@@ -84,6 +84,8 @@ final public class Lua {
         }
     }
     
+    // MARK: - Code interactions
+    
     /// Runs code. Wow.
     /// - Parameters:
     ///   - code: Code to run.
@@ -105,20 +107,27 @@ final public class Lua {
         }
     }
     
-    /// Loads code into memory. Use this function **ONLY** if you know what are you doing.
+    /// Loads code into memory.
     /// - Parameters:
     ///   - code: Code to load.
     ///   - name: Name assigned to this exact piece of code.
-    func loadCode(_ code: String, name: String) throws {
+    public func loadCode(_ code: String, name: String) throws {
         let result = luaL_loadbufferx(L, code, code.utf8.count, name, nil)
         guard result == 0 else {
-            throw CompilationError(message: getLastErrorMessageFromStack())
+            throw SyntaxError(message: getLastErrorMessageFromStack())
         }
     }
     
-    public func insertToStack(_ string: String) {
-        
+    /// Registers a function to be used in Lua. For more documentation look at <doc:Function-import-and-export> article.
+    /// - Parameters:
+    ///   - function: A function to be passed. Note that it should be a C function (exported to C as `@_cdecl`).
+    ///   - name: How this function should be named in Lua.
+    public func registerFunction(_ function: @escaping lua_CFunction, name: String) {
+        lua_pushcfunction(L, function)
+        lua_setglobal(L, name)
     }
+    
+    // MARK: - Stack manipulation
     
     func getLastErrorMessageFromStack() -> String {
         let valueFromStack = lua_tolstring(L, -1, nil)
@@ -132,9 +141,48 @@ final public class Lua {
         return stringMessage
     }
     
-    public func registerFunction(_ function: @escaping lua_CFunction, name: String) {
-        lua_pushcfunction(L, function)
-        lua_setglobal(L, name)
+    func errorCodeHandler(_ code: Int32) throws {
+        switch code {
+            case LUA_ERRSYNTAX:
+                throw SyntaxError(message: getLastErrorMessageFromStack())
+            case LUA_ERRRUN:
+                throw RuntimeError(message: getLastErrorMessageFromStack())
+            case LUA_ERRMEM:
+                throw MemoryError()
+            default: break
+        }
+    }
+    
+    /// Pushes a `String` to the stack
+    /// - Parameter string: A string to be pushed, nothin' strange here
+    public func pushToStack(_ string: String?) {
+        lua_pushstring(L, string)
+    }
+    
+    /// Pushes a `Double` value to the stack
+    /// - Parameter double: A Double to pushed, nothing suprizing
+    public func pushToStack(_ double: Double?) {
+        if let double = double {
+            lua_pushnumber(L, double)
+        } else {
+            lua_pushnil(L)
+        }
+    }
+    
+    /// Pushes a boolean value to the stack
+    /// - Parameter bool: Well, a boolean to be pushed
+    public func pushToStack(_ bool: Bool?) {
+        if let bool = bool {
+            lua_pushboolean(L, bool ? 1 : 0)
+        } else {
+            lua_pushnil(L)
+        }
+    }
+    
+    /// Pushes a nil value to the stack, because, well, no arguments
+    /// were specified here.
+    public func pushToStack() {
+        lua_pushnil(L)
     }
     
     public func getStringFromStack(at location: Int32) throws -> String {
@@ -147,6 +195,31 @@ final public class Lua {
             }
         }
         return stringMessage
+    }
+    
+    // MARK: - Static methods
+    
+    
+    /// A helper function for functions that are exported into Lua. Returns a `Double` value from
+    /// arguments passed to that function in case if it actually exists there.
+    /// - Parameters:
+    ///   - args: A `OpaquePointer` instance that is passed to the caller function.
+    ///   - index: The argument index that should be returned. **Starts from 1.**
+    ///   Please don't bully me, this is the Lua developer, it is him that did this
+    /// - Returns: The wanted `Double` arg.
+    public static func getArgAsDouble(args: OpaquePointer, index: Int32) -> Double {
+        return luaL_checknumber(args, index)
+    }
+    
+    /// A helper function for functions that are exported into Lua. Returns a `String` value from
+    /// arguments passed to that function in case if it actually exists there.
+    /// - Parameters:
+    ///   - args: A `OpaquePointer` instance that is passed to the caller function.
+    ///   - index: The argument index that should be returned. **Starts from 1.**
+    ///   Please don't bully me, this is the Lua developer, it is him that did this
+    /// - Returns: The wanted `String` arg
+    public static func getArgAsString(args: OpaquePointer, index: Int32) -> String {
+        return String(cString: luaL_checkstring(args, index))
     }
     
     deinit {
