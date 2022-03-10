1305b6eb90b3f354c631b3eeb93b96acf6435cfc
GGorAA <ggoraa1029@gmail.com>
2022-03-10 22:53:57 +0200

    feat: updated liblua.h with luaL_checkstring equivalent

--- Sources/liblua/liblua.h
+++ Sources/liblua/liblua.h
@@ -15,8 +15,17 @@
 #include <lualib.h>
 
 // Undefining macros...
+
+/*
+ This comment is for you, a guy that inspects a transpiled
+ Swift header of this module. This section just
+ contains #undef statements, and they don't have an alternative
+ here in Swift, so that's why you don't see them in the Swift
+ version.
+ */
 #undef lua_pushcfunction(L,f)
 #undef lua_pop(L,n)
+#undef luaL_checkstring(L,n)
 
 // ...and replacing them with functions that Swift understands
 void lua_pushcfunction(lua_State *L, lua_CFunction fn) {
@@ -25,6 +34,10 @@ void lua_pushcfunction(lua_State *L, lua_CFunction fn) {
 
 void lua_pop(lua_State *L, int idx) {
     lua_settop(L, -(idx)-1);
+}
+
+const char *luaL_checkstring(lua_State *L, int arg) {
+    luaL_checklstring(L, arg, NULL);
 }
 
 #endif /* liblua_h */
