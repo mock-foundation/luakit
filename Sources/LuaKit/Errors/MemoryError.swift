beff7f79bd85e8b2b7a058cb3db093344c28b46c
GGorAA <ggoraa1029@gmail.com>
2022-03-10 22:58:56 +0200

    feat: error updates, later to be used in LuaKit for reporting Lua errors

---
+++ Sources/LuaKit/Errors/MemoryError.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryError.swift
+//  
+//
+//  Created by Егор Яковенко on 06.03.2022.
+//
+
+public struct MemoryError: Error { }
--- Sources/LuaKit/Errors/SyntaxError.swift
+++ Sources/LuaKit/Errors/SyntaxError.swift
@@ -1,10 +1,10 @@
 //
-//  CompilationError.swift
+//  SyntaxError.swift
 //  
 //
 //  Created by Егор Яковенко on 04.03.2022.
 //
 
-public struct CompilationError: Error {
+public struct SyntaxError: Error {
     public let message: String
 }
